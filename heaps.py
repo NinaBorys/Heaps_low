08ef0ecef351c54e871717fe3f3be93e7e1b5f27
Nina <nina@MacBook-Air-Nina.local>
2016-09-06 16:14:59 +0300

    initial

--- heaps.py
+++ heaps.py
@@ -2,6 +2,8 @@
 # -*- coding:utf-8 -*-
 
 from collections import Counter
+import matplotlib.pyplot as plt
+import csv
 
 def prepare_text(z):
     """Deletes all non-alphabet symbols from text"""
@@ -12,14 +14,45 @@ def prepare_text(z):
             z[i] = ''
     return ''.join(z)
 
-def main():
+def create_dict(text):
+    '''returns list of pairs (step, unique words), dict of unique words from text'''
+    res_dict = dict()
+    points = list()
+    i=0
+    for word in text.split():
+        if word in res_dict:
+            res_dict[word] += 1
+        else:
+            res_dict[word] = 1
+            points.append((i,len(res_dict)))
+        i += 1
+    return points, res_dict
 
+def heaps_plot(points):
+    '''plot from points (tuples)'''
+    plt.scatter(*zip(*points), linewidth=0.5, color='red', marker='s')
+    plt.xlim([-50,250000])
+    plt.ylim([-50,40000])
+    plt.suptitle('heaps low check')
+    plt.show()
+
+def heaps_low_check():
+    '''reaing text from file and checking heaps low + plot'''
     with open('book2.txt', 'r', encoding='utf-8') as f:
         text = f.read()
-
     text = prepare_text(text)
-    result = Counter(text.split()).items()
-    print(result)
+    points, res_dict = create_dict(text)
+
+    #with ('result_file.csv', 'w') as myfile:
+    #    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    #    wr.writerow(points)
+
+    heaps_plot(points)
+
+
+
+def main():
+    heaps_low_check()
 
 
 
