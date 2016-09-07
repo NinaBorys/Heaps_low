311210ae391e2b6bce87f4f2f71a4f563b1abd77
Nina <nina@MacBook-Air-Nina.local>
2016-09-07 18:12:22 +0300

    plot for checking zipf low added

--- heaps.py
+++ heaps.py
@@ -5,6 +5,7 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import csv
 
+
 def prepare_text(z):
     """Deletes all non-alphabet symbols from text"""
     alphabet_str = "абвгдежзийклмнопрстуфхцчшщъыьэюя "
@@ -13,6 +14,7 @@ def prepare_text(z):
         if k not in alphabet_str:
             z[i] = ''
     return ''.join(z)
+
 
 def create_dict(text):
     '''returns list of pairs (step, unique words), dict of unique words from text'''
@@ -26,7 +28,10 @@ def create_dict(text):
             res_dict[word] = 1
             points.append((i,len(res_dict)))
         i += 1
-    return points, res_dict
+    res_list = list(res_dict.values())
+    res_list.sort(reverse=True)
+    return points, res_list
+
 
 def heaps_plot(points):
     '''plot from points (tuples)'''
@@ -36,24 +41,38 @@ def heaps_plot(points):
     plt.suptitle('heaps low check')
     plt.show()
 
-def heaps_low_check():
+
+def heaps_low_check(write_to_file=True):
     '''reaing text from file and checking heaps low + plot'''
     with open('book2.txt', 'r', encoding='utf-8') as f:
         text = f.read()
     text = prepare_text(text)
-    points, res_dict = create_dict(text)
+    points, res_list = create_dict(text)
 
-    #with ('result_file.csv', 'w') as myfile:
-    #    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-    #    wr.writerow(points)
+    if write_to_file:
+        with open('result_file.csv', 'w') as f:
+            wr = csv.writer(f, delimiter='\t')
+            wr.writerows(r for r in points)
+    #plot drowing part
+    #heaps_plot(points)
+    return res_list
 
-    heaps_plot(points)
 
+def zipf_low_check(y):
+    '''checking I zipf low + plot'''
+    x = [i for i in range(0,len(y))]
+    plt.scatter(x,y)
+    plt.xlim([-50,35000])
+    plt.ylim([-50,12000])
+    plt.suptitle('zipf low check')
+    plt.show()
 
 
 def main():
-    heaps_low_check()
+    #d = { 'key1': 3, 'key2': 11, 'key3': 3, 'key14': 6, 'key72': 56, 'key38': 6,'key12': 7, 'key22': 44, 'key33': 9 }
 
+    d = heaps_low_check(write_to_file=False)
+    zipf_low_check(d)
 
 
 if __name__ == '__main__':
